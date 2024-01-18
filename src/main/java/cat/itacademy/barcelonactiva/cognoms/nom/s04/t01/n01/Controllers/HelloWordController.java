08f1e4a44093552013402ec6c6b995ca37fa5f8b
judithgispert <jugispert@gmail.com>
2024-01-18 13:40:27 +0100

    Corrections /helloWorld

--- src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01/Controllers/HelloWordController.java
+++ src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01/Controllers/HelloWordController.java
@@ -3,14 +3,13 @@ package cat.itacademy.barcelonactiva.cognoms.nom.s04.t01.n01.Controllers;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1")
 public class HelloWordController {
-    @GetMapping("/HelloWord")
-    public String greet(@RequestParam(defaultValue = "UNKNOWN") String name){
+    @GetMapping("/HelloWorld")
+    public String greet(@RequestParam(value = "name", defaultValue = "UNKNOWN") String name){
         return "Hello, " + name + ". You're running a Maven project.";
     }
 
-    @GetMapping({"/HelloWord2", "/HelloWord2/{name}"})
+    @GetMapping({"/HelloWorld2", "/HelloWorld2/{name}"})
     public String greet2(@PathVariable(required = false) String name){
         if (name != null){
             return "Hello, " + name + ". You're running a Maven project.";
